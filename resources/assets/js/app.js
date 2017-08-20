aebfd8173f1807debc63e2b9436ae28c84dcdd4e
Tanvir Alam <ta@casalogic.dk>
2017-08-20 22:09:37 +0200

    dummmy stuff for testing

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,48 @@
 import Vue from 'vue'
-
-import store from './store'
+import axios from 'axios'
+import NProgress from 'vue-nprogress'
+import { sync } from 'vuex-router-sync'
+import App from './App.vue'
 import router from './router'
+import store from './store'
+import * as filters from './filters'
+import { TOGGLE_SIDEBAR } from 'vuex-store/mutations'
 
-Vue.component('example', require('./components/Example.vue'))
+Vue.use(NProgress)
+
+Vue.prototype.$http = axios
+Vue.axios = axios
+// Enable devtools
+Vue.config.devtools = true
+
+sync(store, router)
+
+const nprogress = new NProgress({ parent: '.nprogress-container' })
+
+const { state } = store
+
+router.beforeEach((route, redirect, next) => {
+    if (state.app.device.isMobile && state.app.sidebar.opened) {
+    store.commit(TOGGLE_SIDEBAR, false)
+}
+next()
+})
+
+Object.keys(filters).forEach(key => {
+    Vue.filter(key, filters[key])
+})
+
+window.axios = require('axios')
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+}
 
 const app = new Vue({
     el: '#app',
+    router,
     store,
-    router
+    nprogress
 })
+
+export { app, router, store }
