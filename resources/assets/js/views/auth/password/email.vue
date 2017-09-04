8a760c1c7df35540892785f6be6986d0a944e7f0
Tanvir Alam <ta@casalogic.dk>
2017-09-04 22:43:54 +0200

    Vue form for send password reset request

---
+++ resources/assets/js/views/auth/password/email.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+import Form from 'vform'
+
+export default {
+  metaInfo: { titleTemplate: 'Reset Password | %s' },
+
+  data: () => ({
+    status: null,
+    form: new Form({ email: '' })
+  }),
+
+  methods: {
+    send () {
+      this.form.post('/api/password/email')
+        .then(({ data: { status }}) => {
+          this.form.reset()
+          this.status = status
+        })
+    }
+  }
+}
+</script>
