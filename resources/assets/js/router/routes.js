7be5a24b4ae081bb1953cd059a607c2e00fc563a
Tanvir Alam <ta@casalogic.dk>
2017-09-04 22:38:46 +0200

    Basic route for login and registration

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -1,4 +1,9 @@
 export default [
-    { name: 'Login', path: '/', component: require('../views/auth/login.vue'), meta: { title: 'Login' } },
+    { name: 'auth.login', path: '/', component: require('../views/auth/login'), meta: { title: 'Login' } },
+
+    { name: 'auth.register', path: '/auth/register', component: require('../views/auth/register'), meta: { title: 'Register' } },
+
+    { name: 'mfbf', path: '/mfbf-dashboard', component: require('../views/Dashboard'), meta: { title: 'Dashboard' } },
+
     { path: '*', redirect: '/' }
 ]
