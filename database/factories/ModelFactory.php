fe6991bb29689b84807e98a492664ab8f0ddb1f0
Tanvir Alam <ta@casalogic.dk>
2017-09-07 12:31:20 +0200

    Remove username from factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,7 +16,6 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
