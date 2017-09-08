ee0d4ed8924709a34d04f5846fc6438822b3c033
Tanvir Alam <ta@casalogic.dk>
2017-09-09 00:44:52 +0200

    Change the route for different user types

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -1,9 +1,15 @@
+import { authGuard, guestGuard } from '../utils/router'
+
 export default [
     { name: 'auth.login', path: '/', component: require('../views/auth/login'), meta: { title: 'Login' } },
 
-    { name: 'auth.register', path: '/auth/register', component: require('../views/auth/register'), meta: { title: 'Register' } },
+    ...authGuard([
+        { name: 'mfbf', path: '/mfbf-dashboard', component: require('../views/Dashboard'), meta: { title: 'Dashboard' } }
+    ]),
 
-    { name: 'mfbf', path: '/mfbf-dashboard', component: require('../views/Dashboard'), meta: { title: 'Dashboard' } },
+    ...guestGuard([
+        { name: 'auth.register', path: '/auth/register', component: require('../views/auth/register'), meta: { title: 'Register' } }
+    ]),
 
     { path: '*', redirect: '/' }
 ]
