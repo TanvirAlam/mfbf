15cc422c90d7342922f9e7b40f39ba6e9b343aa5
Tanvir Alam <ta@casalogic.dk>
2017-09-17 21:33:24 +0200

    test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    //use DatabaseTransactions;
+
     /** @var \App\Models\User */
     protected $user;
 
@@ -14,28 +17,30 @@ class LoginTest extends TestCase
     {
         parent::setUp();
 
-        //$this->user = factory(User::class)->create();
+        $this->user = factory(User::class)->create();
     }
 
     /** @test */
     public function can_authenticate()
     {
-        /*$this->postJson('/api/login', [
+        $this->postJson('/api/login', [
             'email' => $this->user->email,
             'password' => 'secret',
         ])
         ->assertSuccessful()
         ->assertJsonStructure(['token'])
-        ->assertJson(['token_type' => 'bearer']);*/
+        ->assertJson(['token_type' => 'bearer']);
     }
 
     /** @test */
     public function can_fetch_the_current_user()
     {
-        /*$this->actingAs($this->user)
-            ->getJson('/api/user')
-            ->assertSuccessful()
-            ->assertJsonStructure(['id', 'email']);*/
+        /*$response = $this->actingAs($this->user)
+            ->getJson('/api/user');
+
+        dd($response->content());*/
+            /*->assertSuccessful()
+            ->assertJsonStructure(['email']);*/
     }
 
     /** @test */
@@ -46,10 +51,11 @@ class LoginTest extends TestCase
             'password' => 'secret',
         ])->json()['token'];
 
-        $this->json('POST', "/api/logout?token=$token")
-            ->assertSuccessful();
+        dd($this->json('POST', "/api/logout")->content());*/
+        //dd($this->json('POST', "/api/logout?token=$token")->content());
+            //->assertSuccessful();
 
-        $this->getJson("/api/user?token=$token")
+        /*$this->getJson("/api/user?token=$token")
             ->assertStatus(401);*/
     }
 }
