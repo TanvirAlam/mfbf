0a9e3163c06a39e7263629fd0a41e68077f5c8d2
Tanvir Alam <ta@casalogic.dk>
2017-09-17 19:03:28 +0200

    Have a basic structure for dashboard

--- resources/assets/js/views/Dashboard.vue
+++ resources/assets/js/views/Dashboard.vue
@@ -1,30 +1,42 @@
 <template>
-    <nav class="level is-mobile">
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Tweets</p>
-                <p class="title">3,456</p>
+    <div class="tile is-ancestor">
+        <div class="tile is-vertical is-8">
+            <div class="tile is-horizontal">
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p>Month wise Current Bank Balance with flip card pie chart or bar chart</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p>Month wise total expense and flip card with bar chart</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p>Total saving and flip card chart with detail savings</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p>Here we show my Current Bank Balance with flip card pie chart or bar chart</p>
+                    </article>
+                </div>
             </div>
-        </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Following</p>
-                <p class="title">123</p>
+            <div class="tile is-parent">
+                <article class="tile is-child box">
+                    <p>Tab system here</p>
+                </article>
             </div>
+
         </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Followers</p>
-                <p class="title">456K</p>
-            </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p>Timeline with information on my spending, need a cool design for this</p>
+            </article>
         </div>
-        <div class="level-item has-text-centered">
-            <div>
-                <p class="heading">Likes</p>
-                <p class="title">789</p>
-            </div>
-        </div>
-    </nav>
+    </div>
+
 </template>
 
 <script>
